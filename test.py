105df26cdee8b0177dd48f2aa99fd534e78c2e42
copac2033 <test@test.com>
2021-03-01 22:26:27 +0200

    AM MODIFICAT PRINTAREA

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('Hello World')
+import sys
+
+class hello():
+    def __init__(self):
+        print('Hello World')
+
+    def input(self, args):
+        print('Hello,'+str(args))
+
+if __name__ == "__main__":
+    a = hello()
+    a.input(sys.argv[1])
